9a850bc026d63ee61fa110e00dbaadc4a4b4d89a
z13z <zgogi15@freeuni.edu.ge>
2017-06-01 12:34:28 +0400

    Initial commit

---
+++ src/Web/Serv.java
@@ -0,0 +1,24 @@
+package Web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+/**
+ * Created by z13 on 6/1/17.
+ */
+@WebServlet(value="/Serv",name = "Serv")
+public class Serv extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer=response.getWriter();
+        writer.println("<html><head></head><body>servlet</body><html>");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: z13
+  Date: 6/1/17
+  Time: 11:20 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>hi</title>
+  </head>
+  <body>
+  hello
+  <br>
+  <form action="/Serv" method="get">
+    <input type="submit" value="go">
+  </form>
+  </body>
+</html>
